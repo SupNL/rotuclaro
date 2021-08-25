bfe7afa4f9cb63f69bd35dc610345f75b3ccc3c7
SupNL <gustavoonishinl@gmail.com>
2021-08-25 14:09:41 -0300

    dar setup básico no servidor

---
+++ backend/ormconfig.ts
@@ -0,0 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
+module.exports = [
+    {
+        name: 'default',
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+        synchronize: process.env.SYNCHRONIZE_DB !== 'false',
+        logging: false,
+        entities: ['src/model/**/*.ts'],
+        migrations: ['src/database/migration/**/*.ts'],
+        subscribers: ['src/database/subscriber/**/*.ts'],
+        cli: {
+            entitiesDir: 'src/model',
+            migrationsDir: 'src/database/migration',
+            subscribersDir: 'src/database/subscriber',
+        },
+    },
+];
