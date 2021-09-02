9dd55670a5961fed9e2dbe581b9839b65853adb8
SupNL <gustavoonishinl@gmail.com>
2021-09-02 15:10:28 -0300

    adicionar rota de produto

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import rotaComponente from './subroutes/rotaComponente';
+import rotaProduto from './subroutes/rotaProduto';
 
 const routing = Router();
 
 routing.use('/componente_alergenico', rotaComponente);
+routing.use('/produto', rotaProduto);
 
 export default routing;
