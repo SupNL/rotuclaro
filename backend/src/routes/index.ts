1c719ac57305828b61a9651f199deb1ce1d14e6d
SupNL <gustavoonishinl@gmail.com>
2021-09-06 13:03:53 -0300

    incluir rotas de usuario, perfil e sessões

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
+import { requireAuth } from '../middleware/requireAuth';
 import rotaComponente from './subroutes/rotaComponente';
+import rotaPerfil from './subroutes/rotaPerfil';
 import rotaProduto from './subroutes/rotaProduto';
+import rotaSessao from './subroutes/rotaSessao';
+import rotaUsuario from './subroutes/rotaUsuario';
 
 const routing = Router();
 
-routing.use('/componente_alergenico', rotaComponente);
-routing.use('/produto', rotaProduto);
+routing.use('/componente_alergenico', requireAuth, rotaComponente);
+routing.use('/produto', requireAuth, rotaProduto);
+routing.use('/usuario', rotaUsuario);
+routing.use('/perfil', rotaPerfil);
+routing.use('/sessao', rotaSessao);
 
 export default routing;
