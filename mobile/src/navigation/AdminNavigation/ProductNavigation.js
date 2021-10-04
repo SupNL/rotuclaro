bdc074255ad5ae6024431a674931ce2f594c100d
SupNL <gustavoonishinl@gmail.com>
2021-10-04 20:23:31 -0300

    incluir rotas de produtos

---
+++ mobile/src/navigation/AdminNavigation/ProductNavigation.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import MenuHeaderButton from 'components/MenuHeaderButton';
+import ListProduct from 'screens/Admin/Product/ListProduct';
+import CreateProduct from 'screens/Admin/Product/CreateProduct';
+import EditProduct from 'screens/Admin/Product/EditProduct';
+
+const ProductNavigation = () => {
+    const ComponentStack = createStackNavigator();
+
+    return (
+        <ComponentStack.Navigator
+            initialRouteName='ListProduct'
+            screenOptions={({ navigation }) => ({
+                headerTitle: 'Produtos',
+                headerRight: () => <MenuHeaderButton navigation={navigation} />,
+            })}
+            detachInactiveScreens={true}
+        >
+            <ComponentStack.Screen
+                name='ListProduct'
+                component={ListProduct}
+            />
+            <ComponentStack.Screen 
+                name='CreateProduct'
+                component={CreateProduct}
+                options={{
+                    headerTitle: 'Cadastrar produto'
+                }}
+            />
+            <ComponentStack.Screen 
+                name='EditProduct'
+                component={EditProduct}
+                options={{
+                    headerTitle: 'Alterar produto'
+                }}
+            />
+        </ComponentStack.Navigator>
+    );
+};
+
+export default ProductNavigation;
