3c83f136434f29139f9a5d77355eb977a08785c9
SupNL <gustavoonishinl@gmail.com>
2021-10-04 20:22:17 -0300

    permitir que o admin não seja limitado no produto

--- backend/src/middleware/limitRequests.ts
+++ backend/src/middleware/limitRequests.ts
@@ -1,25 +1,33 @@
 import { Request } from 'express';
 import rateLimit from 'express-rate-limit';
+import { NivelUsuario } from '../model/Usuario';
 
-function customImeiKeyGenerator(req : Request) {
+function customImeiKeyGenerator(req: Request) {
     const idUnico = req.headers.idUnico;
     return `${req.ip}+${idUnico}`;
 }
 
 export const serverLimiter = rateLimit({
     windowMs: 60 * 1000,
-    max: 40
+    max: 40,
 });
 
 export const produtoLimiter = rateLimit({
     windowMs: 10 * 1000,
     max: 1,
-    keyGenerator : customImeiKeyGenerator,
+    keyGenerator: customImeiKeyGenerator,
+    skipFailedRequests: true,
+    skip: (req) => {
+        if (req.usuario) {
+            if (req.usuario.nivel == NivelUsuario.ADMIN) return true;
+        }
+        return false;
+    },
 });
 
 export const accountCreationLimiter = rateLimit({
     windowMs: 2 * 60 * 1000,
     max: 1,
-    keyGenerator : customImeiKeyGenerator,
-    skipFailedRequests : true,
+    keyGenerator: customImeiKeyGenerator,
+    skipFailedRequests: true,
 });
