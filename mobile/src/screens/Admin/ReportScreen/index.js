1142355da85d5c35c7afd2d34044b25d78c8dd10
SupNL <gustavoonishinl@gmail.com>
2021-10-04 20:23:52 -0300

    adicionar mais exemplos

--- mobile/src/screens/Admin/ReportScreen/index.js
+++ mobile/src/screens/Admin/ReportScreen/index.js
@@ -1,20 +1,43 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, View } from 'react-native';
 
 import sharedStyles from 'shared/sharedStyles';
 import ReportContent from 'components/ReportContent';
-import { HeaderTitle } from '@react-navigation/stack';
+import Header3 from 'components/Headers/Header3';
+import CustomText from 'components/CustomText';
 
 const ReportScreen = () => {
     return (
-        <ScrollView contentContainerStyle={sharedStyles.horizontalAlignedScreen}>
-            <ReportContent detailsNavigation={() => {}} title={'Perfis cadastrados'}>
-                <HeaderTitle>114</HeaderTitle>
-            </ReportContent>
-            <ReportContent detailsNavigation={() => {}} title={'Componente alergênico mais evitado'}>
-                <HeaderTitle>Glúten (80 perfis)</HeaderTitle>
-            </ReportContent>
-        </ScrollView>
+        <View style={sharedStyles.horizontalAlignedScreen}>
+            <ScrollView>
+                <ReportContent
+                    detailsNavigation={() => {}}
+                    title={'Perfis cadastrados'}
+                >
+                    <Header3>114</Header3>
+                </ReportContent>
+                <ReportContent
+                    detailsNavigation={() => {}}
+                    title={'Componente alergênico mais evitado'}
+                >
+                    <Header3>Glúten (80 perfis)</Header3>
+                </ReportContent>
+                <ReportContent
+                    detailsNavigation={() => {}}
+                    title={'Consultas realizadas hoje'}
+                >
+                    <Header3>382</Header3>
+                </ReportContent>
+                <ReportContent
+                    detailsNavigation={() => {}}
+                    title={'Produto mais consultado nos últimos três dias'}
+                >
+                    <CustomText style={{ fontWeight: 'bold' }}>
+                        SALGADINHO ELMA CHIPS CHEETOS ONDA REQUEIJAO 37GR
+                    </CustomText>
+                </ReportContent>
+            </ScrollView>
+        </View>
     );
 };
 
